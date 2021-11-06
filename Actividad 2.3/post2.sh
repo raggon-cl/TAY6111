057b333eb0c01a424a9476053e591f02053d581f
Rodrigo Aguilar <raggon@gmail.com>
2021-11-06 12:48:37 -0300

    Script Actividades adicionales

---
+++ Actividad 2.3/post2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+lock_file=/tmp/mysql_tables_read_lock
+###
+mysql_pid=$(cat $lock_file)
+echo "$0 sending sigterm to $mysql_pid" | logger
+pkill -9 -P $mysql_pid
+rm -f $lock_file
+exit 0
